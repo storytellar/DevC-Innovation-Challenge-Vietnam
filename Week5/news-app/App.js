43a5d2d79d01de296052d12f8445b357aa5683b8
storytellar <storytellar12@gmail.com>
2019-10-10 00:59:57 +0700

    Assignment Week 5

---
+++ Week5/news-app/App.js
@@ -0,0 +1,164 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator, FlatList, Linking } from 'react-native';
+import moment from 'moment';
+import { Card, Button } from 'react-native-elements';
+import { Icon } from 'react-native-elements';
+
+export default function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [articles, setArticles] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [hasErrored, setHasApiError] = useState(false);
+  const [lastPageReached, setLastPageReached] = useState(false);
+
+  const getNews = async () => {
+    if (lastPageReached) return;
+    // setLoading(true);
+    try {
+      const response = await fetch(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=86f116199fac4a458457b8898ea85b8f&page=${pageNumber}`
+        // `https://vss.com`
+      );
+      const jsonData = await response.json();
+      const hasMoreArticles = jsonData.articles.length > 0;
+      if (hasMoreArticles) {
+        const newArticleList = filterForUniqueArticles(
+          articles.concat(jsonData.articles)
+        );
+        setArticles(newArticleList);
+        setPageNumber(pageNumber + 1);
+      } else {
+        setLastPageReached(true);
+      }
+      const newArticleList = filterForUniqueArticles(
+        articles.concat(jsonData.articles)
+      );
+      setArticles(newArticleList);
+      setPageNumber(pageNumber + 1);
+    } catch (error) {
+      setHasApiError(true);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  const renderArticleItem = ({ item }) => {
+    return (
+      <Card title={item.title} image={{ uri: item.urlToImage }}>
+        <View style={styles.row}>
+          <Text style={styles.label}>Source</Text>
+          <Text style={styles.info}>{item.source.name}</Text>
+        </View>
+        <Text style={{ marginBottom: 10 }}>{item.content}</Text>
+        <View style={styles.row}>
+          <Text style={styles.label}>Published</Text>
+          <Text style={styles.info}>
+            {moment(item.publishedAt).format('LLL')}
+          </Text>
+        </View>
+        <Button icon={<Icon />} title="Read more" backgroundColor="#03A9F4" onPress={() => onPress(item.url)} />
+      </Card>
+    );
+  };
+
+  const filterForUniqueArticles = arr => {
+    const cleaned = [];
+    arr.forEach(itm => {
+      let unique = true;
+      cleaned.forEach(itm2 => {
+        const isEqual = JSON.stringify(itm) === JSON.stringify(itm2);
+        if (isEqual) unique = false;
+      });
+      if (unique) cleaned.push(itm);
+    });
+    return cleaned;
+  };
+
+  const onPress = url => {
+    Linking.canOpenURL(url).then(supported => {
+      if (supported) {
+        Linking.openURL(url);
+      } else {
+        console.log(`Don't know how to open URL: ${url}`);
+      }
+    });
+  };
+
+  if (hasErrored) {
+    return (
+      <View style={styles.container}>
+        <Text>Error =(</Text>
+      </View>
+    );
+  }
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" loading={loading} />
+      </View>
+    );
+  }
+  else {
+    return (
+      <View style={styles.container}>
+
+        <View style={styles.row}>
+          <Text style={styles.label}>Articles Count:</Text>
+          <Text style={styles.info}>{articles.length}</Text>
+        </View>
+
+        <FlatList
+          data={articles}
+          renderItem={renderArticleItem}
+          keyExtractor={item => item.title}
+
+          onEndReached={getNews} onEndReachedThreshold={1}
+
+          ListFooterComponent={lastPageReached ? <Text>No more articles</Text> : <ActivityIndicator
+            size="large"
+            loading={loading}
+          />}
+        />
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  containerFlex: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  container: {
+    flex: 1,
+    marginTop: 40,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    justifyContent: 'center'
+  },
+  header: {
+    height: 30,
+    width: '100%',
+    backgroundColor: 'pink'
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  label: {
+    fontSize: 16,
+    color: 'black',
+    marginRight: 10,
+    fontWeight: 'bold'
+  },
+  info: {
+    fontSize: 16,
+    color: 'grey'
+  }
+});
